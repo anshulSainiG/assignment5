b1d100309d186011122a824d308c25ae0671a602
Anshul <anshulsaini760@gmail.com>
2023-10-14 14:45:02 +0530

    s

--- src/screens/faq_component/expandable _list_item.tsx
+++ src/screens/faq_component/expandable _list_item.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
